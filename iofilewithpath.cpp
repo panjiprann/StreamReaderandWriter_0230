585f17bd8a38a4dcd8295989fbe823802544f767
panjiprann <panjipranantya20@gmail.com>
2024-06-04 15:00:39 +0700

    menambahkan string nama file dalam iofilewithpath

---
+++ iofilewithpath.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "masukan nama file : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis. 
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + "txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //menambahkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan kararakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' kedalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang menutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
